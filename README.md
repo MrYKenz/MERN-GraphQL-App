1c4e95a9263f91d8cf499b6482f9ed514e5eecec
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-07 18:05:16 +0000

    setting up apollo client and semantic ui

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# MEARN Stack React App with GraphQL API <img src="https://github.com/graphql/graphql-spec/blob/master/resources/GraphQL%20Logo.svg" width="50" />
+# MEARN Stack App with GraphQL API <img src="https://github.com/graphql/graphql-spec/blob/master/resources/GraphQL%20Logo.svg" width="50" />
 - Database: MongoDB
 - Back-end API: Apollo Server (built on Express) with Node.js
 - Front-End: React with Apollo Client
+
+Authentication using JSON Web Tokens and Apollo Client Cache to store fetched results on frontend. Built with React Hooks (including apollo hooks) and the Semantic UI Library.
 
 Based on a similar project I did using python: [Graphene-Flask-API](https://github.com/MrYKenz/Graphene-Flask-API)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+// apollo-link-http to pull graphql data to apollo-client
+const serverUri = createHttpLink({ uri: 'http://localhost:5000'});
+// apollo client constructor with cacheinmemory 
+const client = new ApolloClient({ link: serverUri, cache: new InMemoryCache()})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Start</p>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Router>
+    </ApolloProvider>
   );
 }
 
