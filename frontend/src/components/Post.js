cd681d03814368d08dc85ba608bba16343bbd16b
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-08 18:36:51 +0000

    Post SemanticUI with props from gql query

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, Card, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+const Post = ({post: { id, title, body, username, createdAt }}) => (
+  // let { title, body, username, createdAt } = props.post;
+  <Card.Group>
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+        />
+        <Card.Header as={Link} to={`/posts/${id}`}>{title}</Card.Header>
+        <Card.Meta>{username}</Card.Meta>
+        <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+        <Card.Description>
+          {body}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+          <Button basic color='red'>
+            Delete
+          </Button>
+      </Card.Content>
+    </Card>
+  </Card.Group>
+);
+
+export default Post;
