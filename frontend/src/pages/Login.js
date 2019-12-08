e4d6f3406486b19e9d6d754c1561f8003e837f65
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-08 22:15:15 +0000

    cleaning code

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,41 +5,34 @@ import LoginForm from '../components/LoginForm'
 
 const LOGIN_USER_MUTATION = gql`
     mutation login($username: String! $password: String!) {
-  loginUser(login: {username: $username, password:
-    $password}) {
-    id
-    username
-    createdAt
-    email
-    token
-  }
-}`
+      loginUser(login: {username: $username, password: $password}) 
+      { id username createdAt email token }}`
 
 function Login() {
     const [values, setValues] = useState({ username: '', password: ''});
     const [errors, setErrors] = useState({})
 
     const [LoginUser, { loading }] = useMutation(LOGIN_USER_MUTATION, 
-        { 
-            update(proxy, result) {
-                // console.log(result)
-                window.location.replace("/")
-            }, 
-            onError(err) {
-                // catches gql errors to use in state - present to user
-                // console.log(err.graphQLErrors[0].extensions.exception.errors)
-                setErrors(err.graphQLErrors[0].extensions.exception.errors)
-            },
-            variables: values 
-        });
+      { 
+        update(proxy, result) {
+          // console.log(result)
+          window.location.replace("/")
+        }, 
+        onError(err) {
+          // catches gql errors to use in state - present to user
+          // console.log(err.graphQLErrors[0].extensions.exception.errors)
+          setErrors(err.graphQLErrors[0].extensions.exception.errors)
+        },
+        variables: values 
+      });
 
     const onChange = e => {
-        setValues({ ...values, [e.target.name]: e.target.value })
+      setValues({ ...values, [e.target.name]: e.target.value })
     }
 
     const onSubmit = e => {
-        e.preventDefault();
-            LoginUser()
+      e.preventDefault();
+      LoginUser()
     }
 
     return (
