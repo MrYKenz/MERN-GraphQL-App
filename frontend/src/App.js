96bc501626f07723960d9f3f92ce2a425a1c3b90
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-11 18:26:35 +0000

    redirect away from register & login if auth vallid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
 import { Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
-import { AuthProvider } from './Auth';
+import { AuthProvider, AuthContext } from './Auth';
 
 // apollo-link-http to pull graphql data to apollo-client
 const serverUri = createHttpLink({ uri: 'http://localhost:5000'});
-// apollo client constructor with cacheinmemory 
 const client = new ApolloClient({ link: serverUri, cache: new InMemoryCache()})
 
+// if authenticated redirect away from register & login pages
+const AuthRoute = ({ component: Component, ...prev}) => {
+  const context = useContext(AuthContext);
+  return (<Route {...prev} render={props => context.user ?
+    <Redirect to="/" /> : <Component {...props}/>}
+  />)
+}
 
 function App() {
   return (
@@ -26,12 +32,12 @@ function App() {
           <Router>
             <Header/>
             <Route exact path='/' component={Home}/>
-            <Route exact path='/Login' component={Login}/>
-            <Route exact path='/Register' component={Register}/>
+            <AuthRoute exact path='/Login' component={Login}/>
+            <AuthRoute exact path='/Register' component={Register}/>
           </Router>
         </Container>
       </ApolloProvider>
-      </AuthProvider>
+    </AuthProvider>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,40 +1,58 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Auth';
 
 function Header() {
+  const context = useContext(AuthContext);
+  console.log(context)
   // set active item to url pathname after the '/' except for home
   const path = window.location.pathname === '/' ? 'home' : 
   window.location.pathname.substring(1).toLowerCase();
   const [activeItem, setActiveItem] = useState(path);
 
-  const handleItemClick = (e, { name }) => setActiveItem(name)
+  const onClick = (e, { name }) => setActiveItem(name)
 
-    return (
-      <Menu pointing secondary color="violet">
-        <Menu.Item header>Posts</Menu.Item>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={handleItemClick}
-          as={Link} to="/"
-        />
-        <Menu.Menu position='right'>
-            <Menu.Item
-            name='register'
-            active={activeItem === 'register'}
-            onClick={handleItemClick}
-            as={Link} to="/register"
-            />
-            <Menu.Item
-            name='login'
-            active={activeItem === 'login'}
-            onClick={handleItemClick}
-            as={Link} to="/login"
-            />
-        </Menu.Menu>
-      </Menu>
-    )
+  const menu = context.user ? (
+    <Menu pointing secondary color="violet">
+      <Menu.Item header>Website</Menu.Item>
+      <Menu.Item
+        name={context.user.username}
+        active
+        as={Link} to="/"
+      />
+      <Menu.Menu position='right'>
+          <Menu.Item
+          name='logout'
+          onClick={context.logout}
+          />
+      </Menu.Menu>
+    </Menu>) : (
+    <Menu pointing secondary color="violet">
+      <Menu.Item header>Posts</Menu.Item>
+      <Menu.Item
+        name='home'
+        active={activeItem === 'home'}
+        onClick={onClick}
+        as={Link} to="/"
+      />
+      <Menu.Menu position='right'>
+          <Menu.Item
+          name='register'
+          active={activeItem === 'register'}
+          onClick={onClick}
+          as={Link} to="/register"
+          />
+          <Menu.Item
+          name='login'
+          active={activeItem === 'login'}
+          onClick={onClick}
+          as={Link} to="/login"
+          />
+      </Menu.Menu>
+    </Menu>
+  )
+  return menu;
 }
 
 export default Header;
