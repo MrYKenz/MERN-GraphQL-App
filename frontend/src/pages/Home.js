714c98c413c1338ff5666351d5082a9268b90a48
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-12 21:55:02 +0000

    post form only to show when logged in

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import Post from '../components/Post'
+import Post from '../components/Post';
+import PostForm from '../components/PostForm';
+import { AuthContext } from '../Auth';
 
 const GET_POSTS_QUERY = gql`
     {getPosts {id title body username createdAt}}
 `;
 
 function Home() {
+    const context = useContext(AuthContext);
     const { loading, data } = useQuery(GET_POSTS_QUERY);
     // console.log(loading, data)
     return (
       <div>
+        {context.user && <PostForm />}
         { loading ? <h1>loading...</h1> : (
             data.getPosts && data.getPosts.map(post => 
                 <Post post={post} key={post.id} />)
